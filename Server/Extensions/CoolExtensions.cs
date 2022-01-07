82e58038a005518ef45271527f7d75f0538f54b3
Lyze <11274700+lyze237@users.noreply.github.com>
2022-01-07 21:17:16 +0100

    Changed stdout logs to debug so console isnt spammed in release mode

--- Server/Extensions/CoolExtensions.cs
+++ Server/Extensions/CoolExtensions.cs
@@ -4,6 +4,11 @@ namespace SvgToTvgServer.Server.Extensions
 {
     public static class CoolExtensions
     {
+        public static void LogDebugWhenNotEmpty<T>(this ILogger<T> logger, string msg)
+        {
+            if (!string.IsNullOrWhiteSpace(msg))
+                logger.LogDebug(msg);
+        }
         public static void LogInfoWhenNotEmpty<T>(this ILogger<T> logger, string msg)
         {
             if (!string.IsNullOrWhiteSpace(msg))
